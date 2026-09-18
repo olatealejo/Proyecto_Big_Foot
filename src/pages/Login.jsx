5bf26932413223315a8605282aee9b1d0622240c
olatealejo <alejoolate83@gmail.com>
2026-09-18 16:43:58 -0300

    Dividir login entre formulario y espacio para video

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,10 +3,48 @@ import './Login.css'
 function Login() {
   return (
     <main className="login">
-      <section className="login-contenedor">
-        <h1>Iniciar sesión</h1>
+      <section className="login-video">
+       <video autoPlay muted loop playsInline>
+          <source src="..\assets\video-login.gif" type="video/mp4" />
+        </video>
+      </section>
 
-        <p>Ingresá a tu cuenta de The Big Foot.</p>
+      <section className="login-formulario-contenedor">
+        <div className="login-formulario">
+          <h1>Iniciar sesión</h1>
+
+          <p className="login-descripcion">
+            Ingresá a tu cuenta de The Big Foot.
+          </p>
+
+          <form>
+            <div className="login-campo">
+              <label htmlFor="correo">Correo electrónico</label>
+
+              <input
+                id="correo"
+                name="correo"
+                type="email"
+                placeholder="correo@ejemplo.com"
+              />
+            </div>
+
+            <div className="login-campo">
+              <label htmlFor="contrasena">Contraseña</label>
+
+              <input
+                id="contrasena"
+                name="contrasena"
+                type="password"
+                placeholder="Ingresá tu contraseña"
+              />
+            </div>
+
+            <button type="button" className="login-boton">
+              Ingresar
+            </button>
+          </form>
+        </div>
       </section>
     </main>
   )
