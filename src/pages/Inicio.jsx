a30f266be1577f5a9f92b280bec2c76faff12a82
olatealejo <alejoolate83@gmail.com>
2026-09-18 14:59:12 -0300

    Crear estructura del menu de navegacion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom'
+
 function Inicio() {
   return (
-    <div>
-      <h1>The Big Foot</h1>
-      <p>Página principal</p>
-    </div>
+    <>
+      <nav className="navbar">
+        <Link to="/" className="logo-texto">
+          The Big Foot
+        </Link>
+
+        <div className="navbar-menu">
+          <Link to="/productos">Modelos</Link>
+          <Link to="/nosotros">Confección</Link>
+          <Link to="/">Menú</Link>
+          <Link to="/contacto">Plantillas</Link>
+        </div>
+
+        <div className="navbar-iconos">
+          <button aria-label="Usuario">●</button>
+          <button aria-label="Carrito">▱</button>
+          <button aria-label="Buscar">⌕</button>
+        </div>
+      </nav>
+
+      <main className="inicio"></main>
+    </>
   )
 }
 
