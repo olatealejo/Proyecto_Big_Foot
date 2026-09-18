1d8518cbb6e4554a8cc13764e9a52bc39a798d98
olatealejo <alejoolate83@gmail.com>
2026-09-18 16:31:14 -0300

    Crear estructura visual inicial del login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,13 @@
+import './Login.css'
+
 function Login() {
   return (
-    <main>
+    <main className="login">
+      <section className="login-contenedor">
+        <h1>Iniciar sesión</h1>
+
+        <p>Ingresá a tu cuenta de The Big Foot.</p>
+      </section>
     </main>
   )
 }
