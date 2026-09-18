5adba49969c9cafe968de91bc375991c7a49fbcd
olatealejo <alejoolate83@gmail.com>
2026-09-18 16:04:26 -0300

    Crear estructura base de la pagina login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Inicio from './pages/Inicio'
+import Login from './pages/Login'
 import Productos from './pages/Productos'
 import Nosotros from './pages/Nosotros'
 import Contacto from './pages/Contacto'
@@ -8,6 +9,7 @@ import Contacto from './pages/Contacto'
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Inicio />} />
       <Route path="/productos" element={<Productos />} />
       <Route path="/nosotros" element={<Nosotros />} />
